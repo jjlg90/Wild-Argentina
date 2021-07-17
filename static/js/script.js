d04ad4e1f992f40aa4af023f848cc29c0208b29e
jjlg90 <jjlimag2@gmail.com>
2021-07-17 08:08:35 +0000

    Add js for  view experience image gallery

--- static/js/script.js
+++ static/js/script.js
@@ -52,3 +52,10 @@
         });
     }
 });
+
+function myFunction(imgs) {
+            // Get the expanded image
+            var expandImg = document.getElementById("expandedImg");
+            // Use the same src in the expanded image as the image being clicked on from the grid
+            expandImg.src = imgs.src;
+        }
