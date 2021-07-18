4517b7e8f05371a7b3b0ca9feeae4339bfca4229
jjlg90 <jjlimag2@gmail.com>
2021-07-18 10:23:11 +0000

    Wire google maps API with db data

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,8 +1,27 @@
+//Render Google Maps
 let map;
 
+//Get location form experience
+let lat = document.getElementById("lat").innerHTML;
+let lng = document.getElementById("lng").innerHTML;
+
+//Initialize map
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
+    //Parse lat into float
+    xp_lat = parseFloat(lat);
+    //Parse lng into float
+    xp_lng = parseFloat(lng);
+    //Store coordinates in variable
+    let mapOptions = {
+	    center: { lat: xp_lat, lng: xp_lng },
+	    zoom: 13
+	};
+
+    //Center map on location
+    map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    console.log(typeof xp_lat)
+    console.log(xp_lat)
+    console.log(typeof xp_lng)
+    console.log(xp_lng)
 }
